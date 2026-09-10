3fc342dcb8446296d0164a2bc14cd9250c3935e8
JosephLachowicz <josephlachowicz2@gmail.com>
2026-09-10 14:44:10 -0400

    I wrote code so that my initials would be printed out made of the letters it is.

--- program-1/Assignment1Program1.java
+++ program-1/Assignment1Program1.java
@@ -1,5 +1,15 @@
 public class Assignment1Program1 {
   public static void main(String[] args) {
+    
+    System.out.println("JJJJJJJJJJJJJ    LLL       ");
+    System.out.println("     JJJ         LLL       ");
+    System.out.println("     JJJ         LLL       ");
+    System.out.println("     JJJ         LLL       ");
+    System.out.println("     JJJ         LLL       ");
+    System.out.println("JJJ  JJJ         LLL       ");
+    System.out.println(" JJJ JJJ         LLLLLLLLLL");
+    System.out.println("  JJJJ           LLLLLLLLLL");
 
+    
   }
 }
