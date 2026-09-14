d541d95ef49caffc229f7a9991c8f11cd015612c
JosephLachowicz <josephlachowicz2@gmail.com>
2026-09-14 12:27:23 -0400

    Added another scanner and asked for Fahrenheit. Then saved it as Celsius

--- program-1/Assignment1Program1.java
+++ program-1/Assignment1Program1.java
@@ -3,14 +3,14 @@ import java.util.Scanner;
 public class Assignment1Program1 {
   public static void main(String[] args) {
     
-    System.out.println("JJJJJJJJJJJJJ    LLL       ");
-    System.out.println("     JJJ         LLL       ");
-    System.out.println("     JJJ         LLL       ");
-    System.out.println("     JJJ         LLL       ");
-    System.out.println("     JJJ         LLL       ");
-    System.out.println("JJJ  JJJ         LLL       ");
-    System.out.println(" JJJ JJJ         LLLLLLLLLL");
-    System.out.println("  JJJJ           LLLLLLLLLL");
+    System.out.println("  JJJJJJJJJJJJJ    LLL       ");
+    System.out.println("       JJJ         LLL       ");
+    System.out.println("       JJJ         LLL       ");
+    System.out.println("       JJJ         LLL       ");
+    System.out.println("       JJJ         LLL       ");
+    System.out.println("JJJ    JJJ         LLL       ");
+    System.out.println(" JJJ   JJJ         LLLLLLLLLL");
+    System.out.println("  JJJJJJ           LLLLLLLLLL");
 
     Scanner scanner = new Scanner(System.in);
     System.out.println("Please enter a 5-character string: ");
@@ -18,7 +18,9 @@ public class Assignment1Program1 {
     String middle = input1.substring(1, 4);
     String reverse = new StringBuilder(middle).reverse().toString();
 
+    System.out.println("Please enter a number in Fahrenheit: ");
+    String input2 = scanner.nextLine();
+    String celsius = String.valueOf((Integer.parseInt(input2) - 32) * 5 / 9);
     
-
   }
 }
