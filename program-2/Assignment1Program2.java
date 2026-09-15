de157780d2f0d176bc4aaa244ff63bfb103d4d33
JosephLachowicz <josephlachowicz2@gmail.com>
2026-09-15 15:50:48 -0400

    Made a scanner and did the Math.pow to get the power of the users input

--- program-2/Assignment1Program2.java
+++ program-2/Assignment1Program2.java
@@ -1,5 +1,14 @@
+import java.util.Scanner;
+
 public class Assignment1Program2 {
   public static void main(String[] args) {
+
+    Scanner scanner = new Scanner(System.in);
+    System.out.print("Please enter a base(2 - 9): ");
+    String input = scanner.nextLine();
+
+    int x = (int) Math.pow(Integer.parseInt(input), 4) - 1;
+    System.out.println(x);
 
   }
 }
